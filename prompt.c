cbd4e13e4a340f612214cd28db99abc8fb570d67
jfmoraes <jfsmoraess@gmail.com>
2021-08-18 15:03:43 -0400

    Add Support for Input Editing

--- prompt.c
+++ prompt.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
+
 
 int main(int argc, char** argv){
 	puts("... Lispy Version 0.0.0.0.1");
 	while(1){
-		fputs(">>> ", stdout);
-		fgets(input, 2048, stdin);
+		char* input = readline(">>> ");
+		add_history(input);
+		free(input);
 	}
+	return 0;
 }
