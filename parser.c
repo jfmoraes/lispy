3d32e1b9841bfe7a78ed2bff4e4c24acad826df7
jfmoraes <jfsmoraess@gmail.com>
2021-08-19 16:27:26 -0400

    Undo latest change due to outdated instructions

--- parser.c
+++ parser.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpc.h"
@@ -23,6 +24,18 @@ void add_history(char* unused){}
 #endif
 
 int main(int argc, char** argv){
+
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lispy = mpc_new("lispy");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+		" number   :  /-?[0-9]+/ ; 			   "	  	
+		" operator : '+' | '-' | '*' | '/' ; 		   "	 
+		" expr	  : <number> | '(' <operator> <expr>+ ')'; " 	 
+		" lispy	  : /^/ <operator> <expr>+ /$/; 	   ", 									
+		Number, Operator, Expr, Lispy);
 	puts("... Lispy Version 0.0.0.0.1");
 	while(1){
 		char* input = readline(">>> ");
